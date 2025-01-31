b525ab0a7ae1a2656d1fee62045ebf8e1c891cba
SDI-sodya <saienko.dan06sdi@gmail.com>
2025-01-31 02:21:00 +0200

    Finish 2nd day

---
+++ js/app/pages/campaigns.js
@@ -0,0 +1,16 @@
+export const campaigns = {
+  data:function() {
+    return {
+      parent:"",
+    }
+  },
+  mounted:function() {
+    this.parent = this.$parent.$parent;
+    if(!this.parent.user) {
+      this.parent.logout();
+    }
+  },
+  methods:{},
+  template:`
+  Campaigns`
+};
